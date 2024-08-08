6864ad62dacf3a01e0e26306b833fa716e3a91cd
deepak <deepak@samyotech.com>
2024-08-08 21:11:18 +0530

    middleware added to handle in app.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error : ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+    console.log("MONGO_DB connection failed !!!", error);
+})
 
 
 
